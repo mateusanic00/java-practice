32abf25f164a991ff0856e5eaad4ae1431634adb
Mateus <mateusanicfiorentino@gmail.com>
2025-12-01 22:04:29 -0300

    Simple If/Else statement

---
+++ src/main/java/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main (String[] args){
+        int grade = 8;
+        if (grade > 7) {
+            System.out.println("Approved");
+
+            }
+else {
+                System.out.println("Failed");
+    }
+}
+}
