6e895dbc81b2f26845d9277f8a83727b3d213bfd
Mateus <mateusanicfiorentino@gmail.com>
2025-12-08 00:35:13 -0300

    Create the object 'Opala' and then i called the functions 'Acelerar' and 'Frear'

--- src/main/java/Projects/MainCarro.java
+++ src/main/java/Projects/MainCarro.java
@@ -1,4 +1,9 @@
 package Projects;
 
 public class MainCarro {
+    public static void main (String... args){
+        Carro x = new Carro("Opala", 10);
+        x.Acelerar(197);
+        x.Frear(32);
+    }
 }
