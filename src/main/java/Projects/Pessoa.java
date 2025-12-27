9926d73972c081ac4f7133bc6cdd508f35137bc2
Mateus <mateusanicfiorentino@gmail.com>
2025-12-27 20:48:53 -0300

    Now i added the main variables for the project, and add de constructor

---
+++ src/main/java/Projects/Pessoa.java
@@ -0,0 +1,16 @@
+package Projects;
+
+public class Pessoa {
+    private String idade;
+    private double saldo;
+    private String cpf;
+
+    public Pessoa (String idade, String cpf, double saldo){
+        this.idade = idade;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+    public boolean comprar(int qtd){
+
+    }
+}
