17e425328c941b17878ac37b4c3ec8b9791719a0
Mateus <mateusanicfiorentino@gmail.com>
2025-12-27 21:35:35 -0300

    Change the string idade to string nome, and add the method comprar, to allow people to buy the products we have

--- src/main/java/Projects/Pessoa.java
+++ src/main/java/Projects/Pessoa.java
@@ -1,14 +1,32 @@
 package Projects;
 
 public class Pessoa {
-    private String idade;
+    private String nome;
     private double saldo;
     private String cpf;
 
-    public Pessoa (String idade, String cpf, double saldo){
-        this.idade = idade;
+    public Pessoa (String nome, String cpf, double saldo){
+        this.nome = nome;
         this.cpf = cpf;
         this.saldo = saldo;
+    }
+
+    public boolean comprar (Produto produto, int quantidade){
+        double valorTotal = produto.GetPreco() * quantidade;
+        if (valorTotal > saldo){
+            System.out.println("Saldo insuficiente!");
+            return false;
+        }
+       boolean vendido = produto.Buy(quantidade);
+
+        if (vendido){
+            saldo -= valorTotal;
+            System.out.println("Parabéns " + nome + ", sua compra foi efetuada com sucesso!" );
+            System.out.println("O valor da compra foi de R$" + valorTotal);
+            return true;
+        }
+        System.out.println("Estoque insuficiente, volte mais tarde.");
+        return false;
     }
 
 }
