1a56deaada70f361c225933508d0c0c35627e8b6
Mateus <mateusanicfiorentino@gmail.com>
2025-12-27 21:45:03 -0300

    create main pessoa for tests

--- src/main/java/Projects/mainPessoa.java
+++ src/main/java/Projects/mainPessoa.java
@@ -1,4 +1,10 @@
 package Projects;
 
 public class mainPessoa {
+    public static void main (String... args){
+        Pessoa pessoa1 = new Pessoa("Mateus", "111.111.111.11", 100000);
+        Produto sapato = new Produto("Sapato", 600, 6660);
+        pessoa1.comprar(sapato, 2);
+        pessoa1.ShowSaldo();
+    }
 }
