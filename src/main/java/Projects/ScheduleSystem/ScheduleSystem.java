a75f54a9d9e910ab164f2f6ca1e747f14bff56bc
Mateus <mateusanicfiorentino@gmail.com>
2026-01-12 23:52:11 -0300

    inicializating the array list

--- src/main/java/Projects/ScheduleSystem/ScheduleSystem.java
+++ src/main/java/Projects/ScheduleSystem/ScheduleSystem.java
@@ -1,4 +1,10 @@
 package Projects.ScheduleSystem;
-
+import java.util.ArrayList;
+import java.util.List;
 public class ScheduleSystem {
+    private List<Scheduling> horarios;
+
+    public ScheduleSystem(){
+        horarios = new ArrayList<>();
+    }
 }
