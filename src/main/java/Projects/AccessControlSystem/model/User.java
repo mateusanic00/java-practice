534bd0b5b4d7507b4eed6d85d934c2f997e8b9ed
Mateus <mateusanicfiorentino@gmail.com>
2026-04-30 17:36:42 -0300

    add the list of roles, that will be important for the permissions of the user

--- src/main/java/Projects/AccessControlSystem/model/User.java
+++ src/main/java/Projects/AccessControlSystem/model/User.java
@@ -1,15 +1,21 @@
 package Projects.AccessControlSystem.model;
 
+import java.util.List;
+
 public class User {
     private String user;
     private String senha;
-    Integer id;
-    public User (String user, String senha, Integer id) {
+    private Integer id;
+    private List<Role> roles;
+
+    public User (String user, String senha, Integer id, List<Role> roles) {
         this.user = user;
         this.senha = senha;
         this.id = id;
+        this.roles = roles;
     }
     public String getUser() {return user;}
     public String getSenha() {return senha;}
     public Integer getId() {return id;}
+    public List<Role> getRoles() {return roles;}
 }
