07ccf2522f8dacf693414844cfde3b769a6610a1
Mateus <mateusanicfiorentino@gmail.com>
2026-04-30 17:25:17 -0300

    create the constructor

--- src/main/java/Projects/AccessControlSystem/model/User.java
+++ src/main/java/Projects/AccessControlSystem/model/User.java
@@ -1,4 +1,15 @@
 package Projects.AccessControlSystem.model;
 
 public class User {
+    private String user;
+    private String senha;
+    Integer id;
+    public User (String user, String senha, Integer id) {
+        this.user = user;
+        this.senha = senha;
+        this.id = id;
+    }
+    public String getUser() {return user;}
+    public String getSenha() {return senha;}
+    public Integer getId() {return id;}
 }
