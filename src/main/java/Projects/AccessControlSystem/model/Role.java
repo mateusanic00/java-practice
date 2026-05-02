13b35ddb95c2cd1bd1e1653239cdb482b69379d8
Mateus <mateusanicfiorentino@gmail.com>
2026-05-01 21:46:52 -0300

    create the structure for the class role

--- src/main/java/Projects/AccessControlSystem/model/Role.java
+++ src/main/java/Projects/AccessControlSystem/model/Role.java
@@ -1,4 +1,19 @@
 package Projects.AccessControlSystem.model;
 
+import java.util.List;
+
 public class Role {
+    private Integer id;
+    private String nome;
+    private List<Permission> permissionList;
+
+    public Role (Integer id, String nome, List<Permission> permissionList) {
+        this.id = id;
+        this.nome = nome;
+        this.permissionList = permissionList;
+    }
+
+    public Integer getId() {return id;}
+    public String getNome() {return nome;}
+    public List<Permission> getPermissionList() {return permissionList;}
 }
