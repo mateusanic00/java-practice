06045c587cc95ddb000d1008aa84e168909b5cbe
Mateus <mateusanicfiorentino@gmail.com>
2026-05-01 21:40:47 -0300

    build the class permission

--- src/main/java/Projects/AccessControlSystem/model/Permission.java
+++ src/main/java/Projects/AccessControlSystem/model/Permission.java
@@ -1,4 +1,14 @@
 package Projects.AccessControlSystem.model;
 
 public class Permission {
+    private String resource;
+    private String action;
+
+    public Permission (String resource, String action) {
+        this.resource = resource;
+        this.action = action;
+    }
+
+    public String getResource() {return resource;}
+    public String getAction() {return action;}
 }
